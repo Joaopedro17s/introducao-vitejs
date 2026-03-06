7f3acc93f159ae8c857f0a51099429d74db680f0
Joaopedro17s <joaopedrosoaresgomes13@gmail.com>
2026-03-06 14:43:58 -0300

    aula 06/03

---
+++ src/pages/exemplos/ex-02/index.jsx
@@ -0,0 +1,16 @@
+import styles from './index.module.css'
+
+function Exemplo02 () {
+    return(
+        <div className={styles.containerEx2}>
+          <h1>Exemplo 02</h1>
+          <h2>Estilização CSS</h2>
+          <h3>Uso de Css em diferentes componentes</h3>
+          <p>EU NÃO FIZ NADA</p>
+          <label>Exemplo Label</label>
+          <span>Exemplo Span</span>
+        </div>
+    )
+}
+
+export default Exemplo02;
--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -2,8 +2,6 @@ function Home () {
     return(
         <div>
           <h1>Aula de Vite com React JS</h1>
-          <h3>Estilização CSS</h3>
-          <p>EU NÃO FIZ NADA</p>
         </div>
     )
 }
