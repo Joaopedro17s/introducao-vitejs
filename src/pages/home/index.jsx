d5f3a34b36c373ca4c7def2c4d157804476b071c
Joaopedro17s <joaopedrosoaresgomes13@gmail.com>
2026-03-13 14:23:12 -0300

    Update index.jsx

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,9 +1,20 @@
-function Home () {
+import { useNavigate } from "react-router-dom";
+import {Link} from "react-router"
+    
+function home() {
+
+        const navigate = useNavigate();
     return(
         <div>
           <h1>Aula de Vite com React JS</h1>
+
+          <button onClick={()=> navigate('/exemplo1')}>
+            Exemplo1
+            </button>
+
+            <Link to="/exemplo/2">Exemplo 2</Link>
         </div>
     )
 }
 
-export default Home;
+export default home;
